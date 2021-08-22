e556392498b42833ed690941279ac4b15a9470e7
John <johnlaranjeiras@gmail.com>
2021-08-22 18:36:44 -0300

    meu primeiro commit

---
+++ Professor.py
@@ -0,0 +1,23 @@
+class Professor:
+    def __init__(self, nome, pontuacao):
+        self.__nome = nome
+        self.__pontuacao = pontuacao
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def pontuacao(self):
+        return self.__pontuacao
+
+    @pontuacao.setter
+    def pontuacao(self, pontuacao):
+        self.__pontuacao = pontuacao
+
+
+        
